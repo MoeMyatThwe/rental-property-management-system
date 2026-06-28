47b684579cc0effc30ef1320d42e84b99d83c7b6
Moe Myat Thwe <moemyatthwebts2003@gmail.com>
2026-06-28 21:35:27 +0800

    Add entity layer for user module

--- rental-property-backend/src/main/java/com/nwe/rentalpropertybackend/entity/User.java
+++ rental-property-backend/src/main/java/com/nwe/rentalpropertybackend/entity/User.java
@@ -1,4 +1,65 @@
 package com.nwe.rentalpropertybackend.entity;
 
+import com.nwe.rentalpropertybackend.entity.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(length = 2)
+    private String country;
+
+    @Column(nullable = false)
+    private String countryCode;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate(){
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+
+        if (role == null) {
+            role = UserRole.TENANT;
+        }
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
 }
